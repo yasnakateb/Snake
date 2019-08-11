cc8f4927725d3ba3006986d9c641eafdb21c336a
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-12 04:08:03 +0430

    Added : variables

--- src/snake.c
+++ src/snake.c
@@ -1,6 +1,14 @@
 #include "../include/snake.h"
-#include <ncurses.h>
 
+bool GameOver ;
+int x , y ;
+const int BoxWidth = 120 , BoxHeight = 30 ;
+int score ;
+int food_X , food_Y ;
+enum Directions { STOP = 0 , LEFT , RIGHT , UP , DOWN};
+enum Directions dir;
+int Xtail[TAIL] , Ytail[TAIL];
+int ntail= 0 ;
 
 void Start(){
     initscr();
