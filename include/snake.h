15ba6620def2147b526362dcaad4acc22f50eb95
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-08-12 04:07:12 +0430

    Added : extern

--- include/snake.h
+++ include/snake.h
@@ -4,15 +4,13 @@
 #define TAIL 100
 
 
-bool GameOver ;
-int x , y ;
-const int BoxWidth = 120 , BoxHeight = 30 ;
-int score ;
-int food_X , food_Y ;
-enum Directions { STOP = 0 , LEFT , RIGHT , UP , DOWN};
-enum Directions dir;
-int Xtail[TAIL] , Ytail[TAIL];
-int ntail= 0 ;
+extern bool GameOver;
+extern int x , y;
+const int BoxWidth, BoxHeight;
+extern int score;
+extern int food_X , food_Y;
+extern int Xtail[TAIL] , Ytail[TAIL];
+extern int ntail;
 
 void Start();
 void DrawSnake();
